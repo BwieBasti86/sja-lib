b3c7bc045b048ed835582424df16c7b2661dc44b
BwieBasti86 <sebastian.jacob@gmx.ch>
2021-05-14 13:10:05 +0200

    Added base vue-libary and typescript

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { SjaLibSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  SjaLibSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <sja-lib-sample />
+  </div>
+</template>
---
+++ sja-lib.d.ts
@@ -0,0 +1,7 @@
+import Vue, { PluginFunction, VueConstructor } from 'vue';
+
+
+declare const SjaLib: PluginFunction<any>;
+export default SjaLib;
+
+export const SjaLibSample: VueConstructor<Vue>;
